a3380a47b8dda8fa08c6dbef7872652cf1eaf08f
Guillaume Bagan <guillaume.bagan@liris.cnrs.fr>
2022-06-08 14:30:43 +0200

    ajoute des symboles checked/unchecked pour chaque neuronne de sortie

--- src/Model.purs
+++ src/Model.purs
@@ -12,32 +12,32 @@ import Data.Lens.Record (prop)
 import Data.Maybe (Maybe(..), fromMaybe, maybe)
 import Type.Proxy (Proxy(..))
 
-type Drawing = Array Boolean
+type Pattern = Array Boolean
 
 type Edge 
   = { from :: Int
     , coeff :: Int
     }
 
-data Neuron = Captor Int | From { coeffs :: Array Edge, threshold :: Int }
+data Neuron = Input Int | Neuron { coeffs :: Array Edge, threshold :: Int }
 
-_from :: Prism' Neuron { coeffs :: Array Edge, threshold :: Int }
-_from = prism' From case _ of
-  From e -> Just e
+_neuron :: Prism' Neuron { coeffs :: Array Edge, threshold :: Int }
+_neuron = prism' Neuron case _ of
+  Neuron e -> Just e
   _ -> Nothing
 
 type Model
-  = { drawings :: Array Drawing
+  = { patterns :: Array Pattern
     , neurons :: Array Neuron
     , values :: Array Int
-    , selectedDrawing :: Int
-    , selectedCaptor :: Maybe Int
+    , selectedPattern :: Int
+    , selectedInput :: Maybe Int
     , selectedNeuron :: Int
     , editorOpen :: Boolean
     }
 
-drawingA :: Array Boolean
-drawingA =
+patternA :: Array Boolean
+patternA =
   [ 0, 0, 0, 0, 0, 0, 0, 0, 0
   , 0, 0, 0, 0, 0, 0, 0, 0, 0
   , 0, 0, 0, 0, 1, 1, 0, 0, 0
@@ -49,8 +49,8 @@ drawingA =
   , 0, 0, 0, 0, 0, 0, 0, 0, 0
   ] <#> (_ == 1)
 
-drawingB :: Array Boolean
-drawingB =
+patternB :: Array Boolean
+patternB =
   [ 0, 0, 0, 0, 0, 0, 0, 0, 0
   , 0, 0, 1, 1, 1, 0, 0, 0, 0
   , 0, 0, 1, 0, 1, 0, 0, 0, 0
@@ -62,8 +62,8 @@ drawingB =
   , 0, 0, 0, 0, 0, 0, 0, 0, 0
   ] <#> (_ == 1)
 
-drawingC :: Array Boolean
-drawingC =
+patternC :: Array Boolean
+patternC =
   [ 0, 0, 0, 0, 0, 0, 0, 0, 0
   , 0, 0, 0, 1, 1, 1, 1, 0, 0
   , 0, 0, 1, 0, 0, 0, 0, 0, 0
@@ -75,8 +75,8 @@ drawingC =
   , 0, 0, 0, 0, 0, 0, 0, 0, 0
   ] <#> (_ == 1)
 
-drawingD :: Array Boolean
-drawingD =
+patternD :: Array Boolean
+patternD =
   [ 0, 0, 0, 0, 0, 0, 0, 0, 0
   , 0, 0, 1, 1, 1, 0, 0, 0, 0
   , 0, 1, 1, 0, 0, 1, 0, 0, 0
@@ -90,30 +90,30 @@ drawingD =
 
 init :: Model
 init =
-  { drawings: [ drawingA, drawingB, drawingC, drawingD ]
+  { patterns: [ patternA, patternB, patternC, patternD ]
   , neurons :
-    [ Captor 1, Captor 2, Captor 3, Captor 3, Captor 4, Captor 5 
+    [ Input 0, Input 1, Input 2, Input 3, Input 4, Input 5 
     
-    , From {coeffs: [ {from: 1, coeff: 1}, {from: 4, coeff: 1} ], threshold: 1}
-    , From {coeffs: [ {from: 0, coeff: 1}, {from: 2, coeff: 1} ], threshold: 1}
-    , From {coeffs: [ {from: 2, coeff: 1}, {from: 5, coeff: 1} ], threshold: 1}
-    , From {coeffs: [ {from: 4, coeff: 1}, {from: 5, coeff: 1} ], threshold: 1}
-    , From {coeffs: [ {from: 3, coeff: 1}, {from: 4, coeff: 1} ], threshold: 1}
-    , From {coeffs: [ {from: 1, coeff: 1}, {from: 5, coeff: 1} ], threshold: 1}
+    , Neuron {coeffs: [ {from: 1, coeff: 1}, {from: 4, coeff: 1} ], threshold: 1}
+    , Neuron {coeffs: [ {from: 0, coeff: 1}, {from: 2, coeff: 1} ], threshold: 1}
+    , Neuron {coeffs: [ {from: 2, coeff: 1}, {from: 5, coeff: 1} ], threshold: 1}
+    , Neuron {coeffs: [ {from: 4, coeff: 1}, {from: 5, coeff: 1} ], threshold: 1}
+    , Neuron {coeffs: [ {from: 3, coeff: 1}, {from: 4, coeff: 1} ], threshold: 1}
+    , Neuron {coeffs: [ {from: 1, coeff: 1}, {from: 5, coeff: 1} ], threshold: 1}
     
-    , From {coeffs: [ {from: 6, coeff: 1}, {from: 7, coeff: 1}, {from: 8, coeff: 1} ], threshold: 1}
-    , From {coeffs: [ {from: 6, coeff: 1}, {from: 9, coeff: 1}, {from: 10, coeff: 1} ], threshold: 1}
-    , From {coeffs: [ {from: 7, coeff: 1}, {from: 9, coeff: 1}, {from: 11, coeff: 1} ], threshold: 1}
-    , From {coeffs: [ {from: 8, coeff: 1}, {from: 10, coeff: 1}, {from: 11, coeff: 1} ], threshold: 1}
+    , Neuron {coeffs: [ {from: 6, coeff: 1}, {from: 7, coeff: 1}, {from: 8, coeff: 1} ], threshold: 40}
+    , Neuron {coeffs: [ {from: 6, coeff: 1}, {from: 9, coeff: 1}, {from: 10, coeff: 1} ], threshold: 1}
+    , Neuron {coeffs: [ {from: 7, coeff: 1}, {from: 9, coeff: 1}, {from: 11, coeff: 1} ], threshold: 1}
+    , Neuron {coeffs: [ {from: 8, coeff: 1}, {from: 10, coeff: 1}, {from: 11, coeff: 1} ], threshold: 1}
     ]
   , values: []
-  , selectedDrawing: 0
-  , selectedCaptor: Nothing
+  , selectedPattern: 0
+  , selectedInput: Nothing
   , selectedNeuron: 6
   , editorOpen: false
   } # simulate
 
-countPixels :: Int -> Drawing -> Int
+countPixels :: Int -> Pattern -> Int
 countPixels i =
   length <<< filter identity <<< mapWithIndex (\j b -> 
     let row = j `div` 27
@@ -123,54 +123,54 @@ countPixels i =
   )
 
 simulate :: Model -> Model
-simulate model@{drawings, selectedDrawing, neurons} = model { values = force <$> values }
+simulate model@{patterns, selectedPattern, neurons} = model { values = force <$> values }
   where
     values =
       neurons # mapWithIndex \i neuron -> defer \_ ->
         case neuron of
-          Captor _ -> countPixels i drawing
-          From {coeffs, threshold} ->
+          Input _ -> countPixels i pattern
+          Neuron {coeffs, threshold} ->
             let
               s = sum $ coeffs <#> \{from, coeff} -> coeff * maybe 0 force (values !! from)
             in
               if s >= threshold then s else 0
-    drawing = fromMaybe [] $ drawings !! selectedDrawing
+    pattern = fromMaybe [] $ patterns !! selectedPattern
   
 
 data Msg
-  = SelectCaptor (Maybe Int)
-  | SelectDrawing Int
+  = SelectInput (Maybe Int)
+  | SelectPattern Int
   | SelectNeuron Int
   | ChangeCoeff Int String
   | ChangeThreshold String
-  | OpenDrawingEditor Boolean
+  | OpenPatternEditor Boolean
   | ChangePixel Int
-  | ResetDrawing
+  | ResetPattern
 
 update :: Msg -> Model -> Model
 update msg model = case msg of
-  SelectCaptor i -> model{selectedCaptor = i}
-  SelectDrawing i -> simulate $ model{selectedDrawing = i}
+  SelectInput i -> model{selectedInput = i}
+  SelectPattern i -> simulate $ model{selectedPattern = i}
   SelectNeuron i -> model{selectedNeuron = i}
   ChangeCoeff i s -> case Int.fromString s of
                       Nothing -> model
                       Just val ->
                         simulate $ model # 
-                        (prop (Proxy ∷ _ "neurons") <<< ix model.selectedNeuron <<< _from 
+                        (prop (Proxy ∷ _ "neurons") <<< ix model.selectedNeuron <<< _neuron
                         <<< prop (Proxy :: _ "coeffs") <<< ix i <<< prop (Proxy :: _ "coeff")
                         ) .~ val
   ChangeThreshold s -> case Int.fromString s of
                       Nothing -> model
                       Just val ->
                         simulate $ model # 
-                        (prop (Proxy ∷ _ "neurons") <<< ix model.selectedNeuron <<< _from 
+                        (prop (Proxy ∷ _ "neurons") <<< ix model.selectedNeuron <<< _neuron
                           <<< prop (Proxy :: _ "threshold")
                         ) .~ val
-  OpenDrawingEditor b -> model{editorOpen = b}
-  ChangePixel i -> model # (prop (Proxy ∷ _ "drawings") <<< ix model.selectedDrawing <<< ix i) %~ not
-  ResetDrawing -> model # (prop (Proxy ∷ _ "drawings") <<< ix model.selectedDrawing) .~
-                    case model.selectedDrawing of
-                      0 -> drawingA
-                      1 -> drawingB
-                      2 -> drawingC
-                      _ -> drawingD
+  OpenPatternEditor b -> model{editorOpen = b}
+  ChangePixel i -> simulate $ model # (prop (Proxy ∷ _ "patterns") <<< ix model.selectedPattern <<< ix i) %~ not
+  ResetPattern -> simulate $ model # (prop (Proxy ∷ _ "patterns") <<< ix model.selectedPattern) .~
+                    case model.selectedPattern of
+                      0 -> patternA
+                      1 -> patternB
+                      2 -> patternC
+                      _ -> patternD
--- src/View.purs
+++ src/View.purs
@@ -1,11 +1,14 @@
-module Neuron.View where
+module Neuron.View
+  ( view
+  )
+  where
 
 import Prelude
 
-import Data.Array ((!!), concat, mapWithIndex)
+import Data.Array ((..), (!!), concat, mapWithIndex)
 import Data.Int as Int
 import Data.Maybe (Maybe(..))
-import Neuron.Model (Drawing, Model, Msg(..), Neuron(..))
+import Neuron.Model (Pattern, Model, Msg(..), Neuron(..))
 import Neuron.Util (map2)
 import Pha.Html (Html)
 import Pha.Html as H
@@ -14,7 +17,7 @@ import Pha.Html.Events as E
 import Pha.Html.Util (pc, translate)
 
 showEditor :: Model -> Html Msg
-showEditor {drawings, selectedDrawing} =
+showEditor {patterns, selectedPattern} =
     H.div [H.class_ "absolute flex-center ui-dialog-container"]
     [   H.div [H.class_ "ui-dialog"]
         [   H.div [H.class_ "ui-dialog-head"]
@@ -24,13 +27,13 @@ showEditor {drawings, selectedDrawing} =
         ,   H.div [H.class_ "ui-dialog-buttons"] 
             [   H.button 
                 [   H.class_ "ui-button ui-button-primary"
-                ,   E.onClick \_ -> ResetDrawing
+                ,   E.onClick \_ -> ResetPattern
                 ]
                 [   H.text "Réinitialiser"
                 ]
             ,   H.button
                 [   H.class_ "ui-button ui-button-primary"
-                ,   E.onClick \_ -> OpenDrawingEditor false
+                ,   E.onClick \_ -> OpenPatternEditor false
                 ]
                 [   H.text "Ok"
                 ]
@@ -38,14 +41,14 @@ showEditor {drawings, selectedDrawing} =
         ]
     ]
     where
-    body = H.maybe (drawings !! selectedDrawing) \drawing ->
-        H.div [H.class_ "drawing-editor"] $
-            drawing # mapWithIndex \i b ->
+    body = H.maybe (patterns !! selectedPattern) \pattern ->
+        H.div [H.class_ "pattern-editor"] $
+            pattern # mapWithIndex \i b ->
                 let row = i `div` 9
                     col = i `mod` 9
                 in
                 H.div
-                [   H.class_ "drawing-pixel" 
+                [   H.class_ "pattern-pixel" 
                 ,   H.class' "black" b
                 ,   H.style "width" "11.1111111111%"
                 ,   H.style "height" "11.1111111111%"
@@ -55,14 +58,12 @@ showEditor {drawings, selectedDrawing} =
                 ] []
 
 
-
-
-drawDrawing :: Int -> Boolean -> Maybe Int -> Drawing -> Html Msg
-drawDrawing index selected selectedCaptor  bmap =
+drawPattern :: Int -> Boolean -> Maybe Int -> Pattern -> Html Msg
+drawPattern index selected selectedCaptor  bmap =
     H.div
-    [   H.class_ "drawing"
+    [   H.class_ "pattern"
     ,   H.class' "selected" selected
-    ,   E.onClick \_ -> SelectDrawing index
+    ,   E.onClick \_ -> SelectPattern index
     ] $
         bmap # mapWithIndex \i b->
             let row = i `div` 9
@@ -70,7 +71,7 @@ drawDrawing index selected selectedCaptor  bmap =
                 capt = selected && (Just (row `div` 3) == selectedCaptor || Just (col `div` 3 + 3) == selectedCaptor) 
             in
             H.div
-            [   H.class_ "drawing-pixel"
+            [   H.class_ "pattern-pixel"
             ,   H.class' "black" b
             ,   H.class' "selected" capt
             ,   H.style "width" "11.1111111111%"
@@ -95,17 +96,39 @@ drawLine i j =
     ,   P.stroke "black"
     ]
 
+drawInputLine :: forall a. Int -> Html a
+drawInputLine i =
+    H.line
+    [   P.y1 $ 10.0 + 15.0 * Int.toNumber i
+    ,   P.y2 $ 10.0 + 15.0 * Int.toNumber i
+    ,   P.x1 0.0
+    ,   P.x2 15.0
+    ,   P.strokeWidth 0.5
+    ,   P.stroke "black"
+    ]
+
+drawOutputLine :: forall a. Int -> Html a
+drawOutputLine i =
+    H.line
+    [   P.y1 $ 20.0 + 20.0 * Int.toNumber i 
+    ,   P.y2 $ 20.0 + 20.0 * Int.toNumber i
+    ,   P.x1 105.0
+    ,   P.x2 120.0
+    ,   P.strokeWidth 0.5
+    ,   P.stroke "black"
+    ]    
+
 view :: Model -> Html Msg
-view model@{drawings, neurons, selectedDrawing, selectedCaptor, selectedNeuron, values, editorOpen} =
+view model@{patterns, neurons, selectedPattern, selectedInput, selectedNeuron, values, editorOpen} =
     H.div [H.class_ "layout"]
     [   H.div [H.class_ "row"]
         [   H.div [H.class_ "col-6"]
-            [   H.button [E.onClick \_ -> OpenDrawingEditor true] [H.text "Modifier le motif"]
+            [   H.button [E.onClick \_ -> OpenPatternEditor true] [H.text "Modifier le motif"]
             ,   H.div [] $
-                    drawings # mapWithIndex \i -> drawDrawing i (selectedDrawing == i) selectedCaptor
+                    patterns # mapWithIndex \i -> drawPattern i (selectedPattern == i) selectedInput
             ,   H.maybe (neurons !! selectedNeuron) case _ of
-                    Captor _ -> H.empty
-                    From {coeffs, threshold} ->
+                    Input _ -> H.empty
+                    Neuron {coeffs, threshold} ->
                         H.div [] $
                         [   H.text $ "Seuil: "
                         ,   H.input [P.type_ "number", P.value (show threshold), E.onValueChange ChangeThreshold]
@@ -117,48 +140,74 @@ view model@{drawings, neurons, selectedDrawing, selectedCaptor, selectedNeuron, 
                             ]
                         )
             ]
-        ,   H.div [H.class_ "col-24 relative"]
-            [   H.svg [P.viewBox 0 0 150 100]
-                [   H.g [] $ concat $ neurons # mapWithIndex \i neuron ->
-                            case neuron of
-                                Captor _ -> []
-                                From {coeffs} ->
-                                    coeffs <#> \{from} -> drawLine i from
-                ,   H.g [] $ map2 neurons values \i neuron value ->
-                        let layer = i `div` 6
-                            row = i `mod` 6
-                        in
-                        case neuron of
-                            Captor _ ->
-                                H.g [H.style "transform" $ translate (pc 0.1) (pc $ 0.1 + Int.toNumber row * 0.15)]                        
-                                [   H.circle
-                                    [   P.r 5.0
-                                    ,   E.onPointerEnter \_ -> SelectCaptor (Just i)
-                                    ,   E.onPointerLeave \_ -> SelectCaptor Nothing
-                                    ,   P.fill "pink"
-                                    ]
-                                ,   H.text_ (show value) [P.x (-2.5), P.y 2.0, H.attr "font-size" "0.3rem", H.attr "pointer-events" "none"] 
-                                ]
-                            From {threshold} ->
-                                H.g [H.style "transform" $ translate
-                                    (pc $ if layer == 1 then 0.4 else 0.7)
-                                    (pc $ if layer == 1 then
-                                                          0.1 + 0.15 * Int.toNumber row
-                                                        else
-                                                          0.2 + 0.2 * Int.toNumber row 
-                                    )
-                                ]
-                                [   H.circle
-                                    [   P.r 5.0
-                                    ,   P.fill "pink"
-                                    ,   P.stroke $ if i == selectedNeuron then "green" else "pink"
-                                    ,   E.onClick \_ -> SelectNeuron i
-                                    ]
-                                ,   H.text_ (show value) [P.x (-2.5), P.y 2.0, H.attr "font-size" "0.3rem", H.attr "pointer-events" "none"]
-                                ,   H.text_ ("≥" <> show threshold) [P.x (-2.5), P.y (-5.5), H.attr "font-size" "0.2rem"]
-                                ]
-                ]
-            ]
+        ,   H.div [H.class_ "col-24 relative"] [showNetwork]
+            
         ]
     ,   H.when editorOpen \_ -> showEditor model
     ]
+
+    where
+    showNetwork =
+        H.svg [P.viewBox 0 0 150 100]
+        [   H.g [] $ concat $ neurons # mapWithIndex \i neuron ->
+                    case neuron of
+                        Input _ -> []
+                        Neuron {coeffs} ->
+                            coeffs <#> \{from} -> drawLine i from
+        ,   H.g [] $ drawInputLine <$> (0..5)
+        ,   H.g [] $ drawOutputLine <$> (0..3)
+        ,   H.g [] $ map2 neurons values \i neuron value ->
+                let layer = i `div` 6
+                    row = i `mod` 6
+                in
+                case neuron of
+                    Input _ ->
+                        H.g [H.style "transform" $ translate (pc 0.1) (pc $ 0.1 + Int.toNumber row * 0.15)]                        
+                        [   H.circle
+                            [   P.r 5.0
+                            ,   E.onPointerEnter \_ -> SelectInput (Just i)
+                            ,   E.onPointerLeave \_ -> SelectInput Nothing
+                            ,   P.fill "pink"
+                            ]
+                        ,   H.text_ (show value) [P.x (-2.5), P.y 2.0, H.attr "font-size" "0.3rem", H.attr "pointer-events" "none"] 
+                        ]
+                    Neuron {threshold, coeffs} ->
+                        H.g [H.style "transform" $ translate
+                            (pc $ if layer == 1 then 0.4 else 0.7)
+                            (pc $ if layer == 1 then
+                                                  0.1 + 0.15 * Int.toNumber row
+                                                else
+                                                  0.2 + 0.2 * Int.toNumber row 
+                            )
+                        ]
+                        [   H.circle
+                            [   P.r 5.0
+                            ,   P.fill "pink"
+                            ,   P.stroke $ if i == selectedNeuron then "green" else "pink"
+                            ,   E.onClick \_ -> SelectNeuron i
+                            ]
+                        ,   H.text_ (show value) [P.x (-2.5), P.y 2.0, H.attr "font-size" "0.3rem", H.attr "pointer-events" "none"]
+                        ,   H.text_ ("≥" <> show threshold) [P.x (-2.5), P.y (-5.5), H.attr "font-size" "0.2rem"]
+                        ,   H.g [] $
+                                coeffs # mapWithIndex \j {coeff} ->
+                                    H.text_ ("×" <> show coeff) 
+                                    [   P.x (-7.0)
+                                    ,   P.y (Int.toNumber j * 3.0)
+                                    ,   H.attr "font-size" "0.2rem"
+                                    ,   H.attr "pointer-events" "none"
+                                    ]
+                        ,   H.when (layer == 2) \_ ->
+                                if value >= threshold then
+                                    H.text_ "✓"
+                                    [   P.x 20.0
+                                    ,   H.attr "font-size" "0.4rem"
+                                    ,   P.stroke "green"
+                                    ]
+                                else
+                                    H.text_ "⨯"
+                                    [   P.x 20.0
+                                    ,   H.attr "font-size" "0.4rem"
+                                    ,   P.stroke "red"
+                                    ]
+                        ]
+        ]
