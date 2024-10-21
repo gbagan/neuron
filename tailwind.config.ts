56ba75c1f710aeb4c3032ff6f3741d531be0b3b4
Guillaume Bagan <guillaume.bagan@liris.cnrs.fr>
2024-10-21 16:01:49 +0200

    convert to civet

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from 'tailwindcss';
 
 export default {
-  content: ["./src/**/*.{ts,tsx}"],
+  content: ["./src/**/*.{ts,tsx,civet}"],
   theme: {
     extend: {
       height: {
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite';
+import civet from '@danielx/civet/vite';
 import solidPlugin from 'vite-plugin-solid';
 
 export default defineConfig({
   base: "./",
   plugins: [
+    civet({ ts: "preserve" }),
     solidPlugin(),
   ],
   build: {
